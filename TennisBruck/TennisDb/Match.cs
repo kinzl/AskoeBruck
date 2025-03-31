5a322bb1ffd70924f8bdf393406a42a298ac5e62
kinzl <kinzl.emil@eclipso.at>
2025-03-31 21:03:43 +0200

    new css championship.css

--- TennisBruck/TennisDb/Match.cs
+++ TennisBruck/TennisDb/Match.cs
@@ -3,7 +3,7 @@ namespace TennisDb;
 public class Match
 {
     public int Id { get; set; }
-    public Player Winner { get; set; }
+    public Player? Winner { get; set; }
     public Player Player1 { get; set; }
     public Player Player2 { get; set; }
     public List<Set> Sets { get; set; }
