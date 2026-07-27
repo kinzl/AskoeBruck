1428d15ef6288ea5b33553deff3add3deca13d0c
kinzl <kinzl.emil@eclipso.at>
2026-07-27 14:00:40 +0200

    added migrations

---
+++ TennisBruck/TennisDb/Migrations/20260727114620_AddPartnerToReservation.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TennisDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPartnerToReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PartnerId",
+                table: "Reservations",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_PartnerId",
+                table: "Reservations",
+                column: "PartnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Players_PartnerId",
+                table: "Reservations",
+                column: "PartnerId",
+                principalTable: "Players",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Players_PartnerId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_PartnerId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "PartnerId",
+                table: "Reservations");
+        }
+    }
+}
